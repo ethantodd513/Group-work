b5c1c462914679cfbcc16dde5bc8422b6c6d9420
tawater09 <142839951+tawater09@users.noreply.github.com>
2023-11-06 14:31:18 -0600

    Add files via upload

---
+++ game number thing for etha todd.py
@@ -0,0 +1,63 @@
+import random
+def main():
+    #main accepts no arguments
+    #calls all funtction
+   
+    get_names()
+    name1=random_number
+    while name1!=random_number
+        get_names()
+            if name1>random_number:
+                print("You must guess lower. ")
+            elif name1<random_number:
+                print('You must guess higher.')
+            elif name1=random_number:
+                print("You got it right! The number was  : ",random_number)
+                
+def randomnumber():
+    #creates random number
+    #returns the value
+    random_number=randint(1,1000)
+    return random_number
+    
+
+
+
+
+
+def turns():
+    #makes them take turns
+    turns=0
+    answer=random_number
+    while guess!= answer:
+        guess=input('What is',,"first guess")
+        get_number()
+        
+        if guess !=answer:
+            guess = input("What is",,"guess")
+    else:
+        print("YOU GUESSED RIGHTTTTTT")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
